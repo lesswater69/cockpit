a87b5b0ffdf403a0c05ec14ba5803954bb329235
lesswater69 <56844889+lesswater69@users.noreply.github.com>
2021-06-16 13:38:59 -0400

    Update aws-cli.js

--- examples/aws-cli/aws-cli.js
+++ examples/aws-cli/aws-cli.js
@@ -11,7 +11,7 @@ var bucketname = strResell.concat(hyphen.concat(strORG.concat(strSuffix)));
 
 function awscli_run() {
     //cockpit.spawn(["/bin/aws", "--endpoint-url", "https://s3-west.vmbackup.ca", "s3api", "create-bucket", "--region", "west", "--bucket", reseller.value.toLowerCase(),"-",org_name.value.toLowerCase(),"-o365"])
-    cockpit.spawn(["echo", bucketname)    
+    cockpit.spawn(["echo", bucketname])    
     .stream(awscli_output)
         .then(awscli_success)
         .catch(awscli_fail);
