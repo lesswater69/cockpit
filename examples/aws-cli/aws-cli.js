e93e6610a52c633971f08a3d624813a108924db6
lesswater69 <56844889+lesswater69@users.noreply.github.com>
2021-06-16 12:49:20 -0400

    Update aws-cli.js

--- examples/aws-cli/aws-cli.js
+++ examples/aws-cli/aws-cli.js
@@ -4,32 +4,32 @@ const output = document.getElementById("output");
 const result = document.getElementById("result");
 const button = document.getElementById("create");
 
-function aws-cli_run() {
+function awscli_run() {
     cockpit.spawn(["aws", "--endpoint-url", "https://s3-west.vmbackup.ca", "s3api", "create-bucket", "--region", "west", "--bucket", reseller.value.toLowerCase(),"-",org_name.value.toLowerCase(),"-o365"])
-        .stream(aws-cli_output)
-        .then(aws-cli_success)
-        .catch(aws-cli_fail);
+        .stream(awscli_output)
+        .then(awscli_success)
+        .catch(awscli_fail);
 
     result.innerHTML = "";
     output.innerHTML = "";
 }
 
-function aws-cli_success() {
+function awscli_success() {
     result.style.color = "green";
     result.innerHTML = "success";
 }
 
-function aws-cli_fail() {
+function awscli_fail() {
     result.style.color = "red";
     result.innerHTML = "fail";
 }
 
-function aws-cli_output(data) {
+function awscli_output(data) {
     output.append(document.createTextNode(data));
 }
 
 // Connect the button to starting the "create" process
-button.addEventListener("click", aws-cli_run);
+button.addEventListener("click", awscli_run);
 
 // Send a 'init' message.  This tells integration tests that we are ready to go
 cockpit.transport.wait(function() { });
